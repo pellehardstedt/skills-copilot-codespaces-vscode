16537fe6caae5b687a553d62ce8a5a2d2693b852
pellehardstedt <pelle.hardstedt@gmail.com>
2024-04-25 09:48:43 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,5 @@
 function calculateNumber(var1, var2) {
+    
     return Math.round(var1) + Math.round(var2);
-}
+}; 
+//thanks to the Math.round() function, the result will be rounded to the nearest integer
